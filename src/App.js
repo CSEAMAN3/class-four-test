71f58161ada65d94c116b2fc85b022fbfccc844e
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-07-17 08:58:29 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import Header from "./Header";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    dob: "",
+  });
+
+  function handleFormData(event) {
+    setFormData((prevFormData) => {
+      return { ...prevFormData, [event.target.name]: event.target.value };
+    });
+    console.log(formData);
+  }
+
+  function handleForm(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <form onSubmit={handleForm}>
+        <label htmlFor="name"></label>
+        <input type="text" id="name" name="name" onChange={handleFormData} />
+        <label htmlFor="dob">Date of Birth</label>
+        <input type="date" id="dob" name="dob" onChange={handleFormData} />
+        <button type="submit">Submit</button>
+      </form>
+      <div className="container">
+        <h1>{formData.name}</h1>
+        <p>{formData.dob}</p>
+      </div>
     </div>
   );
 }
